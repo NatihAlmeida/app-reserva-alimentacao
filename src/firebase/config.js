34c0cc87393a929cd627daffd8a7968d633ebaaf
alexop21 <alexandrepontes544@gmail.com>
2026-06-29 21:01:04 -0300

    Ajuste Variaveis

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -11,7 +11,6 @@ const firebaseConfig = {
   appId:             import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId:     import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
-console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
